1889dfe27e09536027aee22e01753e5bc5ddab05
ITPro-Hemedi <hemedicharles@gmail.com>
2025-08-04 16:02:01 +0200

    file updated

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API for managing contacts and books, built for CSE341.'
   },
   host: `localhost:${process.env.PORT || 3000}`,
-  schemes: ['http'],
+  schemes: ['https'],
   tags: [
     {
       name: 'Contacts',
